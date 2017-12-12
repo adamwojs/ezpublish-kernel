c74b7acf267b60dea71b5b3798221957eb64ffbe
Adam Wójs <adam.wojs@ez.no>
2017-12-12 13:21:59 +0100

    fixup! EZP-28480: As a Developer I want API to manage external URLs (PAPI)

--- eZ/Publish/API/Repository/Values/URL/SearchResult.php
+++ eZ/Publish/API/Repository/Values/URL/SearchResult.php
@@ -8,11 +8,15 @@ use eZ\Publish\API\Repository\Values\ValueObject;
 class SearchResult extends ValueObject implements \IteratorAggregate
 {
     /**
+     * The total number of URLs.
+     *
      * @var int
      */
     public $totalCount = 0;
 
     /**
+     * The value objects found for the query.
+     *
      * @var \eZ\Publish\API\Repository\Values\URL\URL[]
      */
     public $items = [];
