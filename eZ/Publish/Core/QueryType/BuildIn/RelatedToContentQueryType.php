dd27086cca20fbbb93b111ccda168b9af00fa25e
Adam Wójs <adam.wojs@ez.no>
2020-02-03 11:23:02 +0100

    EZP-31107: Added eZ\Publish\Core\QueryType\BuildIn\{GeoLocationQueryType,RelatedToContentQueryType} part II

--- eZ/Publish/Core/QueryType/BuildIn/RelatedToContentQueryType.php
+++ eZ/Publish/Core/QueryType/BuildIn/RelatedToContentQueryType.php
@@ -9,7 +9,10 @@ declare(strict_types=1);
 namespace eZ\Publish\Core\QueryType\BuildIn;
 
 use eZ\Publish\API\Repository\Values\Content\Content;
+use eZ\Publish\API\Repository\Values\Content\ContentInfo;
+use eZ\Publish\API\Repository\Values\Content\Field;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\FieldRelation;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,7 +20,7 @@ final class RelatedToContentQueryType extends AbstractQueryType
 {
     public static function getName(): string
     {
-        return 'eZ:RelatedTo';
+        return 'eZ:RelatedToContent';
     }
 
     protected function configureOptions(OptionsResolver $resolver): void
@@ -25,10 +28,20 @@ final class RelatedToContentQueryType extends AbstractQueryType
         parent::configureOptions($resolver);
 
         $resolver->setRequired(['content']);
-        $resolver->setAllowedTypes('content', [Content::class, 'int']);
+        $resolver->setAllowedTypes('content', [Content::class, ContentInfo::class, 'int']);
         $resolver->setNormalizer('content', function (Options $options, $value) {
-            if (is_int($value)) {
-                $value = $this->repository->getContentService()->loadContent($value);
+            if ($value instanceof Content || $value instanceof ContentInfo) {
+                $value = $value->id;
+            }
+
+            return $value;
+        });
+
+        $resolver->setRequired(['field']);
+        $resolver->setAllowedTypes('field', ['string', Field::class]);
+        $resolver->setNormalizer('field', static function (Options $options, $value) {
+            if ($value instanceof Field) {
+                $value = $value->fieldDefIdentifier;
             }
 
             return $value;
@@ -37,7 +50,10 @@ final class RelatedToContentQueryType extends AbstractQueryType
 
     protected function getQueryFilter(array $parameters): Criterion
     {
-        // TODO: Build proper query
-        return new Criterion\MatchNone();
+        return new FieldRelation(
+            $parameters['field'],
+            Criterion\Operator::CONTAINS,
+            $parameters['content']
+        );
     }
 }
